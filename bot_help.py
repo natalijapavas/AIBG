4800fae616316e97490014c52f691a3a8d1f308d
Natalija <natalijapavasovic@gmail.com>
2019-12-07 00:37:16 +0100

    Aleksej sjebao

--- bot_help.py
+++ bot_help.py
@@ -152,16 +152,3 @@ def build(json_res):
 
     return do_action(2,10,'bfa')
 
-res = start_game('13')
-create_game(10,1,2,'mapConfig')
-input('game started, press any key...')
-
-player1 = join_game(10, 1)
-input('1 joined')
-player2 = join_game(10, 2)
-input('2 joined')
-do_action(1, 10, 's')
-input('action 1')
-do_action(2, 10, 'w')
-input('action 2')
-
