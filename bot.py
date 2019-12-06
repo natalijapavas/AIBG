f4dc356415a145b0ad0bcb452a42e52290380337
Natalija <natalijapavasovic@gmail.com>
2019-12-06 20:19:25 +0100

    Basic bot

---
+++ bot.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import requests
+import json
+
+URL = 'http://127.0.0.1:9080'
+GAME_ID = 1
+
+def start_game(playerId):
+    path = '/train/random'
+    data = {'playerId': playerId}
+    print(URL + path)
+    r = requests.get(URL+path, params=data)
+    return r.json()
+
+def join_game(gameId, playerId):
+    path = '/game/play'
+    data = {'playerId', playerId,
+            'gameId', gameId}
+    r = requests.get(URL+path, params=data)
+    return r.json()
+
+res = start_game('13')
+print(res)
+gameData = res
+print(gameData.keys())
