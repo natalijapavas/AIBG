c94e7a3c4088e7453efd9f6a14ea6f9f65c1ca3a
Natalija <natalijapavasovic@gmail.com>
2019-12-06 21:01:52 +0100

    zavrsen API

--- bot.py
+++ bot.py
@@ -1,40 +1,4 @@
 #!/usr/bin/env python3
-import requests
-import json
-
-URL = 'http://127.0.0.1:9080'
-GAME_ID = 1
-
-def start_game(playerId):
-    path = '/train/random'
-    data = {'playerId': playerId}
-    print(URL + path)
-    r = requests.get(URL+path, params=data)
-    return r.json()
-
-def join_game(gameId, playerId):
-    path = '/game/play'
-    data = {'playerId', playerId,
-            'gameId', gameId}
-    r = requests.get(URL+path, params=data)
-    return r.json()
-
-def do_action(playerId, gameId, action):
-    path = '/doAction'
-    data = {'gameId':gameId,
-            'playerId':playerId,
-            'action': action}
-    r = requests.get(URL+path, params=data)
-    return r.json()
-
-def create_game(gameId,playerOne,playerTwo,mapConFig):
-    path = '/admin'
-    data = {'gameId': gameId,
-            'playerOne': playerOne,
-            'playerTwo': playerTwo,
-            'map_name' : mapConFig}
-    r = requests.get(URL+path, params=data)
-    return r.json()
+import bot_help
 
 
-res = start_game('13')
